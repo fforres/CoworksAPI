6a6686852a3ae0798fc9188c00bd52f92b64f8a4
fetorres <fetorres@groupon.com>
2016-08-04 10:04:03 -0400

    TESTING_DEPLOY NEW KEY

--- shipitfile.js
+++ shipitfile.js
@@ -7,7 +7,7 @@ module.exports = function (shipit) {
   shipit.initConfig({
     default: {
       workspace: '/tmp/coworks_api',
-      deployTo: '/var/www/coworks_api',
+      deployTo: '/var/www',
       repositoryUrl: 'git@github.com:fforres/CoworksAPI.git',
       branch: 'master',
       ignores: ['.git', 'node_modules'],
@@ -25,20 +25,5 @@ module.exports = function (shipit) {
     development: {
       servers: 'localhost',
     },
-  });
-  shipit.task('build', function () {
-    shipit.log(chalk.green('Building Production/Dist code (npm run build)'));
-    return shipit.local('npm run build', {cwd: CWD});
-  });
-  shipit.task('copy_source', ['build'], function () {
-    shipit.log(chalk.green('Copying sourcecode to: ' + shipit.config.deployTo));
-    shipit.remoteCopy(CWD, shipit.config.deployTo)
-  })
-  shipit.task('remote_install', ['copy_source'], function () {
-    shipit.log(chalk.green('Copying sourcecode to: ' + shipit.config.deployTo));
-    return shipit.remote('cd /var/www/coworks_api && NODE_ENV=production && npm install');
-  });
-  shipit.task('start', ['remote_install'], function () {
-    return shipit.remote('cd /var/www/coworks_api && NODE_ENV=production && node dist');
   });
 };
