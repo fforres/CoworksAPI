f6a53d1de8d62179bc1e69da3a8a374d0f8e43a0
fetorres <fetorres@groupon.com>
2016-08-04 15:42:35 -0400

    TESTING_DEPLOY NEW KEY

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ express().use('/', cors(), graphqlHTTP({
   schema,
   pretty: true,
 })).listen(3009);
-console.log('Listening on port: 3009')
+console.log('Listening on port: 3009');
+
 if (process.env.NODE_ENV === 'development') {
   express().use('/', graphqlHTTP({
     schema,
     pretty: true,
     graphiql: true,
   })).listen(3010);
-  console.log('Listening on port: 3010')
+  console.log('Listening on port: 3010');
 }
